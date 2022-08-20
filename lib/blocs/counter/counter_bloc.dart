1f4a31dc9c112b35360212f2396c9ad696ce4346
galihasmarandaru <galih.asmarandaru@gmail.com>
2022-08-20 22:53:11 +0700

    add bloc, event, state and using bloclistener, watch, and read

---
+++ lib/blocs/counter/counter_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState.initial()) {
+    on<IncrementCounterEvent>(_onIncrementCounterEvent);
+    on<DecrementCounterEvent>(_onDecrementCounterEvent);
+  }
+
+  _onIncrementCounterEvent(
+    IncrementCounterEvent event,
+    Emitter<CounterState> emit,
+  ) {
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  _onDecrementCounterEvent(
+    DecrementCounterEvent event,
+    Emitter<CounterState> emit,
+  ) {
+    emit(state.copyWith(counter: state.counter - 1));
+  }
+}
